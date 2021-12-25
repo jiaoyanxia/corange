6063800b129edbcad3bc2ad6d5cc2d288b1786c2
freelaeder <yanglo67@qq.com>
2021-12-25 19:49:06 +0800

    完成登录页面

---
+++ src/components/unde.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bgimg">
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped >
+.bgimg{
+    position:fixed;
+    top: 0;
+    left: 0;
+    width:100%;
+    height:100%;
+    min-width: 1000px;
+    z-index:-10;
+    zoom: 1;
+    background-color: #fff;
+    background: url('../assets/u3.gif') no-repeat;
+    background-size: cover;
+    -webkit-background-size: cover;
+    -o-background-size: cover;
+    background-position: center 0;
+}
+
+
+</style>
---
+++ src/views/layout/layout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+
+    <!-- 路径 -->
+    <router-link to="/layout/home">home</router-link>
+    <router-link to="/layout/chat">chat</router-link>
+    <router-link to="/layout/rank">rank</router-link>
+    <router-link to="/layout/info">info</router-link>
+    <router-link to="/layout/about">about</router-link>
+
+
+    <!-- 视图 -->
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+};
+</script>
+
+<style lang="less">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin-right: 10px;
+  a {
+    margin-right: 10px;
+  }
+}
+</style>
